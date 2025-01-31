ec6541eafaad88b5992591ef9e1787f68a79dff2
itsYashes <yashasravindra007@gmail.com>
2025-01-31 18:23:51 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -14,4 +14,12 @@ app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+// routes
+
+import router from "./routes/user.routes.js";
+// routes declaration
+app.use("/api/v1/users", router)
+// http://localhost:8000/api/v1/users/register
+
+
 export { app }
