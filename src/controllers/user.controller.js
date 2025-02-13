875a08491beb6d454d768f9669b5ea28974bfe14
itsYashes <yashasravindra007@gmail.com>
2025-02-13 12:34:00 +0530

    fix all register controller bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -16,18 +16,18 @@ const registerUser =  asyncHandler( async (req, res) => {
     // check for user creation
     // return response
 
-    const {fullname, email, username, password} = req.body
-    console.log("email: ", email);
+    const {fullName, email, username, password} = req.body
+    // console.log("email: ", email);
 
     // if (fullname === "") {
     //     throw new ApiError(400, "Full name is required")
     // } Can do for every variable
 
-    if ([fullname, email, username, password].some((field) => field?.trim() === "")) {
+    if ([fullName, email, username, password].some((field) => field?.trim() === "")) {
         throw new ApiError(400, "All fields are required")
     }
 
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [{ email }, { username }]
     })
 
@@ -36,7 +36,14 @@ const registerUser =  asyncHandler( async (req, res) => {
     }
 
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0 ) {
+        coverImageLocalPath = req.files.coverImage[0].path
+    } else {
+        
+    } 
 
     if (!avatarLocalPath) {
         throw new ApiError(400, "Avatar file is required")
@@ -50,7 +57,7 @@ const registerUser =  asyncHandler( async (req, res) => {
     }
 
     const user = await User.create({
-        fullname,
+        fullName,
         avatar: avatar.url,
         coverImage: coverImage?.url || "",
         email,
